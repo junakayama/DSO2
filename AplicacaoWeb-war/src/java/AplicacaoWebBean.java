d1825c114b5a0026e6eab6ab15d530c96c0a68ad
leondaros <leondaros@users.noreply.github.com>
2016-10-16 22:43:35 -0200

    Início do desenvolvimento da lógica

--- AplicacaoWeb-war/src/java/AplicacaoWebBean.java
+++ AplicacaoWeb-war/src/java/AplicacaoWebBean.java
@@ -5,6 +5,10 @@
  */
 
 import ejb.AplicacaoWebService;
+import ejb.Consultas;
+import ejb.Pacientes;
+import java.util.ArrayList;
+import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
 import javax.inject.Named;
 import javax.enterprise.context.RequestScoped;
@@ -20,10 +24,74 @@ public class AplicacaoWebBean {
     @EJB
     private AplicacaoWebService aplicacaoWebService;
 
-    /**
-     * Creates a new instance of AplicacaoWebBean
-     */
+    private ArrayList<Consultas> consultasLista;
+    
+    private ArrayList<Consultas> listaConsultasDisponiveis;
+    
+    private Consultas pesquisaConsulta;
+    
     public AplicacaoWebBean() {
+    }
+
+    @PostConstruct
+    public void init(){
+        
+        //RECEBE A LISTA DE CONSULTAS DAQUELE PACIENTE
+        consultasLista=aplicacaoWebService.buscaConsultasMarcadas(Integer.SIZE);
+    }
+    
+    public void cancelaConsulta(Integer codc,Integer codp){
+        Pacientes p = aplicacaoWebService.buscaPaciente(codp);
+        Consultas c = aplicacaoWebService.buscaConsulta(codc);
+        p.getConsultasCollection().remove(c);
+        c.setCodpac(null);
+        aplicacaoWebService.merge(c);
+        aplicacaoWebService.merge(p);
+    }
+    
+    public void marcaConsulta(Integer codc,Integer codp){
+        Pacientes p = aplicacaoWebService.buscaPaciente(codp);
+        Consultas c = aplicacaoWebService.buscaConsulta(codc);
+        p.getConsultasCollection().add(c);
+        c.setCodpac(p);
+        aplicacaoWebService.merge(c);
+        aplicacaoWebService.merge(p);
+    }
+
+    public void consultasDisponiveis(){
+        listaConsultasDisponiveis=aplicacaoWebService.pesquisaConsultas(pesquisaConsulta);
+    }
+
+    public ArrayList<Consultas> getListaConsultasDisponiveis() {
+        return listaConsultasDisponiveis;
+    }
+
+    public void setListaConsultasDisponiveis(ArrayList<Consultas> listaConsultasDisponiveis) {
+        this.listaConsultasDisponiveis = listaConsultasDisponiveis;
+    }
+    
+    public Consultas getPesquisaConsulta() {
+        return pesquisaConsulta;
+    }
+
+    public void setPesquisaConsulta(Consultas pesquisaConsulta) {
+        this.pesquisaConsulta = pesquisaConsulta;
+    }
+    
+    public AplicacaoWebService getAplicacaoWebService() {
+        return aplicacaoWebService;
+    }
+
+    public void setAplicacaoWebService(AplicacaoWebService aplicacaoWebService) {
+        this.aplicacaoWebService = aplicacaoWebService;
+    }
+
+    public ArrayList<Consultas> getConsultasLista() {
+        return consultasLista;
+    }
+
+    public void setConsultasLista(ArrayList<Consultas> consultasLista) {
+        this.consultasLista = consultasLista;
     }
     
 }
